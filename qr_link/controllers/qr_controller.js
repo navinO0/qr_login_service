06d6f49b8e28b4429dd20fe8e84e069d93a78d21
navinO0 <6y.navin@gmail.com>
2025-09-14 19:22:23 +0530

    fixed id for the newly registered users

--- qr_link/controllers/qr_controller.js
+++ qr_link/controllers/qr_controller.js
@@ -170,9 +170,10 @@ async function REGISTER_GOOGLE_AUTH(request, reply) {
         const body = request.body;
         const {device_info} = request.body
         const { username, email,first_name, profile_photo } = decryptObject(this, body,['username','email','first_name']);
-        const user = await getUserDetails(this, username)
-        const token = await genereateToke(this, {username, email, first_name, id : user.id}, device_info)
+        let user = await getUserDetails(this, username)
+        let token;
         if (user && user !== "") {
+            token = await genereateToke(this, {username, email, first_name, id : user.id}, device_info)
            return replySuccess(reply, { message: "User already registered" , token : token})
         }
 
@@ -184,7 +185,8 @@ async function REGISTER_GOOGLE_AUTH(request, reply) {
             password : ''
         };
 
-        await createUser(this, userDetails);
+        user = await createUser(this, userDetails);
+        token = await genereateToke(this, {username, email, first_name, id : user.id}, device_info)
         return replySuccess(reply, { message: 'success', token : token })
     } catch (err) {
         return replyError(reply, err)
